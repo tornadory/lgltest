047b9bd8307d5225b9df858fa0da2b13376f53be
AliZhang <tornadory@gmail.com>
2018-10-21 11:50:28 +0800

    saoa

--- demo/SimpleThreeJsExample/asset/js/three-helper.js
+++ demo/SimpleThreeJsExample/asset/js/three-helper.js
@@ -58,8 +58,9 @@ const ThreeHelper = function(){
     this.targetVideo.onplay = ()=>{
         console.log("start to playing ");
         this.targetVideo.style.display = 'none';
-        // this.movieScreen.visible = true;
-        this.movieScreen.material.transparent = true ;
+        this.movieScreen.visible = true;
+        this.movieScreen.material.transparent = false;
+        this.movieScreen.material.opacity = 1;
     }
     this.videoTexture = new THREE.VideoTexture(this.targetVideo);
     this.videoTexture.wrapS = this.videoTexture.wrapT = THREE.ClampToEdgeWrapping;
@@ -69,13 +70,14 @@ const ThreeHelper = function(){
     this.movieMaterial = new THREE.MeshBasicMaterial({
         map: this.videoTexture,
         side: THREE.DoubleSide,
-        transparent: true
+        transparent: true,
+        opacity: 0.01
     });
 
     this.movieGeometry = new THREE.BoxGeometry(25, 15, 7);
     this.movieScreen = new THREE.Mesh(this.movieGeometry, this.movieMaterial);
     this.movieScreen.position.set(0, 0, 0);
-    // this.movieScreen.visible = false;
+    this.movieScreen.visible = false;
     this.scene.add(this.movieScreen);
 
     this.render();
