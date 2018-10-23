855ab5c46286efead14957371f1719d0a6f374f1
zhangli <li_zhang@ssic.cn>
2018-10-23 17:40:53 +0800

    啊

--- demo/webar/js/three-helper.js
+++ demo/webar/js/three-helper.js
@@ -2,12 +2,15 @@
  * ThreeJS帮助类
  * @constructor
  */
-const ThreeHelper = function(){
+const ThreeHelper = function () {
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.camera.position.set(0, 0, 25);
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    this.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+    });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.domElement.setAttribute('class', 'mainCanvas');
     document.body.appendChild(this.renderer.domElement);
@@ -25,14 +28,14 @@ const ThreeHelper = function(){
         this.camera.aspect = window.innerWidth / window.innerHeight;
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-        if (window.innerWidth < window.innerHeight){
-            this.movieScreen.scale.set(10,5,2);
-        }else{
-            this.movieScreen.scale.set(25,15,7);
+        if (window.innerWidth < window.innerHeight) {
+            this.movieScreen.scale.set(10, 5, 2);
+        } else {
+            this.movieScreen.scale.set(25, 15, 7);
         }
     }, false);
 
-    this.render = function() {
+    this.render = function () {
         this.renderer.render(this.scene, this.camera);
 
         for (const mixer of this.mixers) {
@@ -44,7 +47,7 @@ const ThreeHelper = function(){
         });
     };
 
-    this.loadObject = function(modelUrl) {
+    this.loadObject = function (modelUrl) {
         const loader = new THREE.FBXLoader();
         loader.load(modelUrl, (object) => {
             object.scale.setScalar(0.02);
@@ -59,18 +62,18 @@ const ThreeHelper = function(){
         })
     };
 
-    this.targetVideo = document.getElementById( 'targetVideo' );
-    this.targetVideo.onplay = ()=>{
+    this.targetVideo = document.getElementById('targetVideo');
+    this.targetVideo.onplay = () => {
         console.log("start to playing ");
         this.targetVideo.style.display = 'none';
         this.movieScreen.visible = true;
-        
+
         this.movieScreen.material.map.needsUpdate = true;
     }
 
-    
+
     this.videoTexture = new THREE.VideoTexture(this.targetVideo);
-    this.videoTexture.wrapS = this.videoTexture.wrapT = THREE.ClampToEdgeWrapping; 
+    this.videoTexture.wrapS = this.videoTexture.wrapT = THREE.ClampToEdgeWrapping;
     this.videoTexture.minFilter = THREE.LinearFilter;
     this.videoTexture.magFilter = THREE.LinearFilter;
 
@@ -82,14 +85,13 @@ const ThreeHelper = function(){
     this.movieGeometry = new THREE.BoxGeometry(1, 1, 1);
     this.movieScreen = new THREE.Mesh(this.movieGeometry, this.movieMaterial);
     this.movieScreen.position.set(0, 0, 0);
-    this.movieScreen.visible = false;
-    if (window.innerWidth < window.innerHeight){
-        this.movieScreen.scale.set(10,5,2);
-    }else{
-        this.movieScreen.scale.set(25,15,7);
+    // this.movieScreen.visible = false;
+    if (window.innerWidth < window.innerHeight) {
+        this.movieScreen.scale.set(10, 5, 2);
+    } else {
+        this.movieScreen.scale.set(25, 15, 7);
     }
     this.scene.add(this.movieScreen);
 
     this.render();
 };
-
